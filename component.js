9f4a4d5bb77041120d826d4657d2269b89848595
Jan B <jan@rancher.com>
2016-11-16 12:07:09 -0500

    Create component.js

---
+++ component.js
@@ -0,0 +1,202 @@
+/* v----- Do not change anything between here
+ *       (the DRIVERNAME placeholder will be automatically replaced during build) */
+define('ui/components/machine/driver-OTC/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
+
+  exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
+    driverName: 'OTC',
+/* ^--- And here */
+
+    // Write your component here, starting with setting 'model' to a machine with your config populated
+    bootstrap: function() {
+      let config = this.get('store').createRecord({
+        type        : 'OTCConfig',
+        size        : 512,
+      });
+
+      this.set('model', this.get('store').createRecord({
+        type: 'machine',
+        'OTCConfig': config,
+      }));
+    },
+
+    // Add custom validation beyond what can be done from the config API schema
+    validate() {
+      // Get generic API validation errors
+      this._super();
+      var errors = this.get('errors')||[];
+
+      // Add more specific errors
+
+      // Check something and add an error entry if it fails:
+      if ( parseInt(this.get('model.OTCConfig.size'),10) < 1024 )
+      {
+        errors.push('Size must be at least 1024 MB');
+      }
+
+      // Set the array of errors for display,
+      // and return true if saving should continue.
+      if ( errors.get('length') )
+      {
+        this.set('errors', errors);
+        return false;
+      }
+      else
+      {
+        this.set('errors', null);
+        return true;
+      }
+    },
+
+    // Any computed properties or custom logic can go here
+  });
+});
+;
+define("ui/components/machine/driver-OTC/template",["exports","ember","ui/mixins/driver"],function(exports,_ember,_uiMixinsDriver){
+
+exports["default"] = Ember.HTMLBars.template((function() {
+  return {
+    meta: {
+      "revision": "Ember@2.9.1",
+      "loc": {
+        "source": null,
+        "start": {
+          "line": 1,
+          "column": 0
+        },
+        "end": {
+          "line": 36,
+          "column": 0
+        }
+      }
+    },
+    isEmpty: false,
+    arity: 0,
+    cachedFragment: null,
+    hasRendered: false,
+    buildFragment: function buildFragment(dom) {
+      var el0 = dom.createDocumentFragment();
+      var el1 = dom.createElement("section");
+      dom.setAttribute(el1,"class","horizontal-form");
+      var el2 = dom.createTextNode("\n  ");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createElement("div");
+      dom.setAttribute(el2,"class","container-fluid");
+      var el3 = dom.createTextNode("\n");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createComment("");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n\n    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createElement("div");
+      dom.setAttribute(el3,"class","over-hr r-mt20 r-mb20");
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("span");
+      var el5 = dom.createTextNode("My Skeleton Driver");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n    ");
+      dom.appendChild(el3, el4);
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n\n");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createElement("div");
+      dom.setAttribute(el3,"class","row");
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-sm-12 col-md-2 form-label");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createElement("label");
+      dom.setAttribute(el5,"class","form-control-static");
+      var el6 = dom.createTextNode("Size");
+      dom.appendChild(el5, el6);
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-sm-12 col-md-4");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createElement("div");
+      dom.setAttribute(el5,"class","input-group");
+      var el6 = dom.createTextNode("\n          ");
+      dom.appendChild(el5, el6);
+      var el6 = dom.createComment("");
+      dom.appendChild(el5, el6);
+      var el6 = dom.createTextNode("\n          ");
+      dom.appendChild(el5, el6);
+      var el6 = dom.createElement("span");
+      dom.setAttribute(el6,"class","input-group-addon");
+      var el7 = dom.createTextNode("MB");
+      dom.appendChild(el6, el7);
+      dom.appendChild(el5, el6);
+      var el6 = dom.createTextNode("\n        ");
+      dom.appendChild(el5, el6);
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n    ");
+      dom.appendChild(el3, el4);
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n\n");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createComment("");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n  ");
+      dom.appendChild(el2, el3);
+      dom.appendChild(el1, el2);
+      var el2 = dom.createTextNode("\n\n");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createTextNode("  ");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createComment("");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createTextNode("\n\n");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createTextNode("  ");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createComment("");
+      dom.appendChild(el1, el2);
+      var el2 = dom.createTextNode("\n");
+      dom.appendChild(el1, el2);
+      dom.appendChild(el0, el1);
+      var el1 = dom.createTextNode("\n");
+      dom.appendChild(el0, el1);
+      return el0;
+    },
+    buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+      var element0 = dom.childAt(fragment, [0]);
+      var element1 = dom.childAt(element0, [1]);
+      var morphs = new Array(5);
+      morphs[0] = dom.createMorphAt(element1,2,2);
+      morphs[1] = dom.createMorphAt(dom.childAt(element1, [7, 3, 1]),1,1);
+      morphs[2] = dom.createMorphAt(element1,10,10);
+      morphs[3] = dom.createMorphAt(element0,4,4);
+      morphs[4] = dom.createMorphAt(element0,7,7);
+      return morphs;
+    },
+    statements: [
+      ["inline","partial",["host/add-common"],[],["loc",[null,[4,4],[4,33]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","value",["subexpr","@mut",[["get","model.OTCConfig.size",["loc",[null,[19,18],[19,38]]],0,0,0,0]],[],[],0,0]],["loc",[null,[17,10],[20,12]]],0,0],
+      ["inline","partial",["host/add-options"],[],["loc",[null,[27,4],[27,34]]],0,0],
+      ["inline","top-errors",[],["errors",["subexpr","@mut",[["get","errors",["loc",[null,[31,22],[31,28]]],0,0,0,0]],[],[],0,0]],["loc",[null,[31,2],[31,30]]],0,0],
+      ["inline","save-cancel",[],["save","save","cancel","cancel"],["loc",[null,[34,2],[34,45]]],0,0]
+    ],
+    locals: [],
+    templates: []
+  };
+}()));;
+
+});
